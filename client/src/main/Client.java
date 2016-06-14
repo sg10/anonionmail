d41bf3ded55bd32f43cd5446917b1d05837d6bb0
Philipp Schuster <p.schuster@student.tugraz.at>
2016-06-14 11:13:34 +0200

    fixed error

--- client/src/main/Client.java
+++ client/src/main/Client.java
@@ -131,9 +131,7 @@ public class Client
 	private void printOptions()
 	{
 		System.out.println("Command list: ");
-		System.out.print( "\t-publicKey\tfor requesting the servers public key (needed for all other commands)\n" +
-				//"\t-login\t\tfor logging into the server with alias and password\n"+
-				"\t-alias\t\tfor requesting an new alias\n" +
+		System.out.print( "\t-alias\t\tfor requesting an new alias\n" +
 				"\t-send\t\tfor sending a message to another user\n" +
 				"\t-fetch\t\tfor fetching all your mails from the server\n" +
 				"\t-exit\t\tfor exiting this application\n\n");
