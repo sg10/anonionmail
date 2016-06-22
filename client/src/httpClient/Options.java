4256093ff21029634edc6096f2dd7204bb6a5c26
Philipp Schuster <p.schuster@student.tugraz.at>
2016-06-22 20:20:19 +0200

    fixed proxy address

--- client/src/httpClient/Options.java
+++ client/src/httpClient/Options.java
@@ -2,7 +2,7 @@ package httpClient;
 
 public class Options
 {
-	public static String PROXY_ADDRESS = "5r2xccfnims3y23w.onion";//"https://v3-dot-anonionmail.appspot.com/";//"5r2xccfnims3y23w.onion";//"http://localhost:8080/";
+	public static String PROXY_ADDRESS = "http://5r2xccfnims3y23w.onion/";//"https://v3-dot-anonionmail.appspot.com/";//"5r2xccfnims3y23w.onion";//"http://localhost:8080/";
 	public static String SERVER_ADDRESS = "v3-dot-anonionmail.appspot.com";
 	public static String REQUEST_ALIAS = "request_alias.txt";
 	public static String REQUEST_LOGIN = "login-request.txt";
