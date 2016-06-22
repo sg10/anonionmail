a475960f45c08d0da13ff4548053144791df7488
sg10 <sgruber@student.tugraz.at>
2016-06-22 21:22:00 +0200

    comments in launch script

--- vm/anonionmail-launch.sh
+++ vm/anonionmail-launch.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# -----
+# this script can be used to easily download and start the script (e.g. within Tails)
+
 if [ ! -f anonionmail.jar ]; then
   wget https://github.com/sg10/akits1-anonionmail/raw/master/client-tor.jar -O anonionmail.jar
 fi
 
+# start java app with sockets configured for Tor
 java -jar anonionmail.jar -DsocksPort=9050 -DsocksHost=127.0.0.1
